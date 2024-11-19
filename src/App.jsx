bc11801846a08cdbe6a0181d3e9a162738513cb7
tanmay agrawal <tanmay@tanmays-MacBook-Air.local>
2024-11-19 11:07:01 +0530

    added pages, did the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import { BrowserRouter,Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 export default function App() {
-  return  <h1 className = 'text-red-500'>Tanmay</h1>;
-  
+  return  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/sign-in" element={<SignIn />} />
+    <Route path="/sign-up" element={<SignUp />} />
+    <Route path="/profile" element={<Profile/>} />
+    
+
+
+  </Routes>
+  </BrowserRouter>;
 }
