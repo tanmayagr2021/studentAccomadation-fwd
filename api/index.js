e617f0b8c7a812eccaa80341f656e16e02e9f014
tanmay agrawal <tanmay@tanmays-MacBook-Air.local>
2024-11-19 21:38:08 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
-const app = express();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+const app = express();
+mongoose.connect(process.env.MONGO).then (() => {
+console.log("COnnected to MOngoDB");
+}).catch((err)=>{
+    console.log(err);
+})
 app.listen(5174, () => {
     console.log("Server is running on port 5174");
 }
