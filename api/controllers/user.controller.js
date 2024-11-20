c5ed3d6cd0bf10e73c7ccf4efa5a8f0700010914
tanmay agrawal <tanmay@tanmays-MacBook-Air.local>
2024-11-20 16:51:37 +0530

    created signup api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,5 @@
 export const test = (req,res)=> {
 res.json({
-    message: "api routes arre orking"
+    message: "api routes are working"
 });
 }
--- api/index.js
+++ api/index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 mongoose
 .connect(process.env.MONGO)
 .then (() => {
@@ -18,3 +20,4 @@ app.listen(5174, () => {
 );
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
