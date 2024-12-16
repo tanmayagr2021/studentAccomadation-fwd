c03250c2b5ced5ad365c14505a2b731bf448c812
tanmay agrawal <tanmay@tanmays-MacBook-Air.local>
2024-12-16 21:48:26 +0530

    google UTH

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -16,28 +16,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
 );
 
-
-
-
-
-// // createRoot(document.getElementById('root')).render(
-// //   <StrictMode>
-// //     <App />
-// //   </StrictMode>,
-// // )
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App.jsx';
-// import { store, persistor } from './redux/store.js'; // Import `persistor` here
-// import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/integration/react';
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <Provider store={store}>
-//     <PersistGate loading={null} persistor={persistor}>
-//       <App />
-//     </PersistGate>
-//   </Provider>
-// );
